5f5ce4d88cfbc9c78ec30f18be16e7e72ed603d0
Savinien <drsavitruc@hotmail.fr>
2023-03-01 14:54:17 +0100

    capteur fonctionnel

--- Nucleo-Arduino-Pio/src/main.cpp
+++ Nucleo-Arduino-Pio/src/main.cpp
@@ -1,9 +1,28 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Adafruit_MLX90614.h>
+
+Adafruit_MLX90614 capteurTempCiel = Adafruit_MLX90614(); //par défaut addr=0x5A
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("Adafruit MLX90614 Emissivity Setter.\n");
+
+  // init sensor
+  if (!capteurTempCiel.begin()) //il faut re-init si on coupe l'alimentation au capteur (en premiere approche)
+  {
+    Serial.println("Error connecting to MLX sensor. Check wiring.");
+    while (1);
+  };
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.print("Temperature Objet:"); //celle à utiliser avec capteur pointé vers le ciel
+  Serial.println(capteurTempCiel.readObjectTempC());
+  Serial.print("Temperature Ambiente:");
+  Serial.println(capteurTempCiel.readAmbientTempC());
+  delay(1000);
+
 }
