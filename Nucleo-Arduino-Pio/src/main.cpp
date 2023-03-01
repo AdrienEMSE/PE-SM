0f89a59a80d34913967e13e1aea03e93beca27b1
Adrien <adrien.komaroff@etu.emse.fr>
2023-03-01 14:36:18 +0100

    removed useless code

--- Nucleo-Arduino-Pio/src/main.cpp
+++ Nucleo-Arduino-Pio/src/main.cpp
@@ -26,25 +26,6 @@ static void smartDelay(unsigned long ms)
   } while (millis() - start < ms);
 }
 
-static void printFloat(float val, bool valid, int len, int prec)
-{
-  if (!valid)
-  {
-    while (len-- > 1)
-      Serial.print('*');
-    Serial.print(' ');
-  }
-  else
-  {
-    Serial.print(val, prec);
-    int vi = abs((int)val);
-    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
-    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
-    for (int i=flen; i<len; ++i)
-      Serial.print(' ');
-  }
-  smartDelay(0);
-}
 
 static void printInt(unsigned long val, bool valid, int len)
 {
@@ -106,7 +87,7 @@ void setup()
 
 void loop()
 {
-  static const double LONDON_LAT = 51.508131, LONDON_LON = -0.128002;
+ ;
 
 
   printDateTime(gps.date, gps.time);
