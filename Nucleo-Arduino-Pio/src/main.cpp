bc4a098aceaa64e97ba303acc13366b66a3ebdbc
Adrien <adrien.komaroff@etu.emse.fr>
2023-03-02 11:05:31 +0100

    RH OK

--- Nucleo-Arduino-Pio/src/main.cpp
+++ Nucleo-Arduino-Pio/src/main.cpp
@@ -1,11 +1,22 @@
 #include <Arduino.h>
 #include <Adafruit_CCS811.h>            // include library for CCS811 - Sensor from martin-pennings https://github.com/maarten-pennings/CCS811
 #include <Adafruit_BMP280.h>            // include main library for BMP280 - Sensor
+#include "ClosedCube_HDC1080.h"
+
+
 #include <Wire.h>                       // This library allows you to communicate with I2C
 
 Adafruit_CCS811 ccs;
 Adafruit_BMP280 bmp280;                // I2C
+ClosedCube_HDC1080 hdc1080;
 
+void printSerialNumber() {
+	Serial.print("Device Serial Number=");
+	HDC1080_SerialNumber sernum = hdc1080.readSerialNumber();
+	char format[12];
+	sprintf(format, "%02X-%04X-%04X", sernum.serialFirst, sernum.serialMid, sernum.serialLast);
+	Serial.println(format);
+}
 
 void setup() {
 //  
@@ -27,6 +38,20 @@ void setup() {
     while (true);
   }
 
+	Serial.println("ClosedCube HDC1080 Arduino Test");
+
+	// Default settings: 
+	//  - Heater off
+	//  - 14 bit Temperature and Humidity Measurement Resolutions
+	hdc1080.begin(0x40);
+
+	Serial.print("Manufacturer ID=0x");
+	Serial.println(hdc1080.readManufacturerId(), HEX); // 0x5449 ID of Texas Instruments
+	Serial.print("Device ID=0x");
+	Serial.println(hdc1080.readDeviceId(), HEX); // 0x1050 ID of the device
+	
+	printSerialNumber();
+ 
 
   
 }
@@ -40,6 +65,12 @@ void loop() {
   Serial.print("Pressure = ");
   Serial.print(bmp280.readPressure() / 100);
   Serial.println(" Pa, ");
+
+	Serial.print("T=");
+	Serial.print(hdc1080.readTemperature());
+	Serial.print("C, RH=");
+	Serial.print(hdc1080.readHumidity());
+	Serial.println("%");
 
 
 
