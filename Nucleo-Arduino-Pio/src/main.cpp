aad624f8c31b31a879f54b9a12ba62cab4a92a60
Adrien <adrien.komaroff@etu.emse.fr>
2023-03-02 09:51:22 +0100

    rainOmeter Ok

--- Nucleo-Arduino-Pio/src/main.cpp
+++ Nucleo-Arduino-Pio/src/main.cpp
@@ -6,54 +6,41 @@
 Adafruit_CCS811 ccs;
 Adafruit_BMP280 bmp280;                // I2C
 
+uint32_t pinPluvioAnalog = A0;
+uint32_t pinPluvioGPIO = D7;
+float seuil_haut = 815.0; // no rain
+float seuil_bas = 425.0; // full rain
 
 void setup() {
 //  
   Serial.begin(9600);
-  // Enable I2C
-  Wire.begin();                  // put here the Pins of I2C
-  Serial.println("CCS811 test");      /* --- SETUP CCS811 on 0x5A ------ */
-  if(!ccs.begin()){
-    Serial.println("Failed to start sensor! Please check your wiring.");
-    while(1);
-  }
-  while(!ccs.available());
+  pinMode(pinPluvioAnalog,INPUT_ANALOG);
+  pinMode(pinPluvioGPIO,INPUT_PULLDOWN);
 
 
 
-  Serial.println("BMP280 test");     /* --- SETUP BMP on 0x76 ------ */
-  if (!bmp280.begin(0x76)) {
-    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
-    while (true);
-  }
 
 
   
 }
 
 void loop() {
+
+  uint32_t rain_read = analogRead(pinPluvioAnalog);
+  int rain_GPIO = digitalRead(pinPluvioGPIO);
+  float rainPercent = (seuil_haut/(seuil_haut-seuil_bas)) -((float)rain_read)/(seuil_haut-seuil_bas);
+  if(rainPercent > 1.0)
+    rainPercent = 1.0;
+  else if (rainPercent < 0.0)
+    rainPercent = 0.0;
+  Serial.print("rain_raw :");
+  Serial.print(rain_read);
+  Serial.print(" GPIO : ");
+  Serial.print(!rain_GPIO); // read GPIO at 1 when no rain, 0 when rain
+  Serial.print(" rain % : ");
+  Serial.println( rainPercent );
+
+  delay(1000);
   
-  Serial.print("BMP280 => Temperature = ");
-  Serial.print(bmp280.readTemperature());
-  Serial.print(" °C, ");
 
-  Serial.print("Pressure = ");
-  Serial.print(bmp280.readPressure() / 100);
-  Serial.println(" Pa, ");
-
-
-
-  if(ccs.available()){
-    if(!ccs.readData()){
-      Serial.print("CO2: ");
-      Serial.print(ccs.geteCO2());
-      Serial.print("ppm, TVOC: ");
-      Serial.println(ccs.getTVOC());
-    }
-    else{
-      Serial.println("ERROR!");
-      while(1);
-    }
-  }
-  delay(5000);
 }
