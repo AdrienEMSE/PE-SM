db809ff00fdf7b6d7efb5cacffb469ab602e38da
Savinien <drsavitruc@hotmail.fr>
2023-03-02 09:16:09 +0100

    capteur fonctionnel

--- Nucleo-Arduino-Pio/src/main.cpp
+++ Nucleo-Arduino-Pio/src/main.cpp
@@ -1,9 +1,19 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include "Adafruit_VEML6070.h"
+
+Adafruit_VEML6070 capteurUV = Adafruit_VEML6070(); //une résistance de RSET=270kOhms est soudée sur le capteur
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("VEML6070 Test");
+  capteurUV.begin(VEML6070_1_T);  // clear ACK's et écrit 0x06 dans le registre de commande conformement au mode d'emploi
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  
+  Serial.print("UV light level: "); Serial.println(capteurUV.readUV()); //pour interpréter: https://www.vishay.com/docs/84310/designingveml6070.pdf page 5
+  capteurUV.sleep(true); //diminue la conso à 1 microA
+  delay(1000);
+  capteurUV.sleep(false); //pas besoin de réinitialiser
+  }
