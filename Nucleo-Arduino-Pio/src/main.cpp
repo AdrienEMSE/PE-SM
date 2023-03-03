388f2b878e428fdda2ae8d899d1dc690cff40094
Adrien <adrien.komaroff@etu.emse.fr>
2023-03-03 14:38:59 +0100

    Revert "test"
    This reverts commit 91f736de96b436d706b10980af67c7b41052b489.

--- Nucleo-Arduino-Pio/src/main.cpp
+++ Nucleo-Arduino-Pio/src/main.cpp
@@ -1,15 +1,8 @@
 
 #include <Arduino.h>
 #include <Wire.h>                       // This library allows you to communicate with I2C
-#include <TinyGPS++.h>
-#include <SoftwareSerial.h>
-
-static const int RXPin = 4, TXPin = 7; // PIN Liaison série GPS
-static const uint32_t GPSBaud = 9600;  // BAUD GPS
 
 
-TinyGPSPlus gps; // GPS
-SoftwareSerial ss(RXPin, TXPin); // Liaison série vers GPS
 
 
 void setup() {
