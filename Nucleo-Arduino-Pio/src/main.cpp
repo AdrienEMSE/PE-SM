7eda593fc08960f2946afdc8cc22d3c7d2126b9a
Savinien <drsavitruc@hotmail.fr>
2023-03-10 13:25:39 +0100

    PLLN 216 => 192 [temps de démarrage de malade]

--- Nucleo-Arduino-Pio/src/main.cpp
+++ Nucleo-Arduino-Pio/src/main.cpp
@@ -438,7 +438,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   RCC_OscInitStruct.PLL.PLLM = 16;
-  RCC_OscInitStruct.PLL.PLLN = 216;
+  RCC_OscInitStruct.PLL.PLLN = 192;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
   RCC_OscInitStruct.PLL.PLLQ = 2;
   RCC_OscInitStruct.PLL.PLLR = 2;
